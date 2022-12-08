627a43dabbb24288cf4ebf5ee3664fd7ea437ac2
Tope Jimoh <jimohtope2012@gmail.com>
2022-12-08 12:43:48 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - Entry block
- * Return: 0
+ * main - main block
+ * Return: o
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
