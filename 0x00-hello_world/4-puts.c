4446b3a3992989f01662c1b015ae467c310057b8
Tope Jimoh <jimohtope2012@gmail.com>
2022-12-08 07:31:53 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio>
+
+/**
+ * main - Entry point 
+ * Return: Always 0 (success)
+ * /
+ 
+ int main (void)
+ {
+ 	puts("\" Programming is like building a multilingual puzzle");
+		return (0);
+}
