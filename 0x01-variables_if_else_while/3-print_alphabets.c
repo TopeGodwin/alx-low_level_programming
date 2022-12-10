7e909698a9d3873178267ba3001890746ea480f6
Tope Jimoh <jimohtope2012@gmail.com>
2022-12-09 16:16:20 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - putting Alphabets to be in lowercase then after make it uppercase
+ * Return: 0
+ */
+int main(void)
+{
+	char letter; 
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
+	
+	putchar('\n');
+
+	return (0); 
+}
