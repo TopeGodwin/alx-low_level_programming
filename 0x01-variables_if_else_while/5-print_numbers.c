2d5e6a9119f552b221ba498c773a279797631653
Tope Jimoh <jimohtope2012@gmail.com>
2022-12-09 16:50:59 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Printing of single numbers of base 10 where you can start from 0
+ * Return: 0
+ */
+int main(void)
+{
+	int num; 
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+	printf('\n');
+	return (0);
+}
