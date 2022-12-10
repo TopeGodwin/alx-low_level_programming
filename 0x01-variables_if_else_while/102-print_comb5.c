5b59efcf02c7aff3332d33a2957d8616261904a2
Tope Jimoh <jimohtope2012@gmail.com>
2022-12-09 19:39:16 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,7 +12,7 @@ int main (void)
 		for (num2 = num1; num2 <= 99; num2++)
 		{
 			putchar((num1 / 10) + '0');
-			putchar((num1 % 10) + '0') 
+			putchar((num1 % 10) + '0');
 			putchar(' ');
 			putchar((num2 / 10) + '0');
 			putchar((numw % 10) + '0');
