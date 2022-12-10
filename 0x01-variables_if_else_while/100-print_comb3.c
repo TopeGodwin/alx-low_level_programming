223ee3787631c4ea23db408f3710b0bd77395a5c
Tope Jimoh <jimohtope2012@gmail.com>
2022-12-09 19:12:56 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,7 +6,7 @@
 int main(void)
 {
 	int digit1, digit2;
-	
+
 	for (digit1 = 0; digit1 < 9; digit1++)
 	{
 		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
@@ -14,7 +14,7 @@ int main(void)
 			putchar((digit1 % 10) + '0');
 			putchar((digit2 % 10) + '0');
 
-			if (digit1 == 8 && digit2 ==9)
+			if (digit1 == 8 && digit2 == 9)
 				continue;
 
 			putchar(',');
