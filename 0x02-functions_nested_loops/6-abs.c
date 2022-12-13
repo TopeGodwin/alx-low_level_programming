7136fb5b7b4a08254c49f571c5c10a1a453d5a92
Tope Jimoh <jimohtope2012@gmail.com>
2022-12-13 15:54:34 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - calculate tje absotlute value of an integer
+ * @n: The intergerwill be calculated
+ * Return: n,-n 
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else 
+		return (-n);
+}
