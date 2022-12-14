85fb7ffc2e6fb6fd18a51739eb2aa39937ed5d0d
Tope Jimoh <jimohtope2012@gmail.com>
2022-12-13 19:00:27 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,13 +9,35 @@
 void print_to_98(int n)
 {
 	if (n < 98)
-		{
+	{
 		while (n <= 98)
-		
+		{
 			printf("%d", n);
 			if (n != 98)
 			{
-			printf(",");
+				printf(",");
 			}
-			n++
+			n++; 
+		}
+	}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(",");
+			}
+			n--;
+		}
+	}
+	else
+	{
+		printf("98");
+	}
+	printf("\n");
+
+}
+
 	
