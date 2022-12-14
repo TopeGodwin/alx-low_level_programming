aafb25e09ceb9a5e5117f7358abf06380f059b9f
Tope Jimoh <jimohtope2012@gmail.com>
2022-12-13 18:26:09 -0800

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+  * print_times_table - Entry point
+  * @n: input
+  * Return: 0
+  */
+void print_times_table(int n)
+{
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * print_to_98 - a programm that will print natural number n-98
+  * @n: number input
+  * Return: 0
+  */
+void print_to_98(int n)
+{
+	if (n < 98)
+		{
+		while (n <= 98)
+		
+			printf("%d", n);
+			if (n != 98)
+			{
+			printf(",");
+			}
+			n++
+	
