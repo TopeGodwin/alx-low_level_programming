080b1640e460b8fa186274a98ba4c1f5db7c6548
Tope Jimoh <jimohtope2012@gmail.com>
2022-12-14 14:02:56 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,7 +10,7 @@ void print_times_table(int n)
 {
 	int x, y, z;
 
-	if (n >=0 && n <= 14)
+	if (n >= 0 && n <= 14)
 	{
 		for (x = 0; x <= n; x++)
 		{
@@ -21,7 +21,7 @@ void print_times_table(int n)
 				{
 					_putchar(',');
 					_putchar(32);
-					_putchar((z / 10) + '0');
+					_putchar((z / 100) + '0');
 					_putchar(((z / 10) % 10) + '0');
 					_putchar((z % 10)  + '0');
 				}
@@ -46,9 +46,6 @@ void print_times_table(int n)
 				}
 			}
 			_putchar('\n');
-
 		}
 	}
 }
-	
-
