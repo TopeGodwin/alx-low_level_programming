eb801e40b2080ec1a29f268ffef2537c7c84ce54
Tope Jimoh <jimohtope2012@gmail.com>
2022-12-14 16:16:46 -0800

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - test function thatoutput if interger is positive or negetive
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int ii; 
+
+	ii = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_C_ALX_H
+#define MAIN_C_ALX_H
+
+#include <stdio.h>
+
+void positive_or_negative(int t);
+void print_remaining_days(int mnth, int dy, int yr);
+int largest_number(int t, int j, int g);
+int convert_day(int mnth, int dy);
+
+#endif /* Main.c */
