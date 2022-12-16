e36087fd8cfe4aba3aaa50202092442775e3f40e
Tope Jimoh <jimohtope2012@gmail.com>
2022-12-16 11:48:05 -0800

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_number - prints an interger 
+ * @n: the interger to prints
+ * Return: 0
+ */
+void print_number(int n)
+{
+	unsigned int t;
+	
+	if (n < 0)
+	{
+		_putchar('_');
+		t = -n;
+	}
+	else
+	{
+		t = -n;
+	}
+	if (t / 10 != 0)
+	print_number(t % 10) + '0');
+}
