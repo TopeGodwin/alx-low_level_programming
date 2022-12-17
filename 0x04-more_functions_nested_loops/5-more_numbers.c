c2fc773360164a25f27a60c81bdb16b6faa0c17b
Tope Jimoh <jimohtope2012@gmail.com>
2022-12-16 16:43:15 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,15 +13,12 @@ void more_numbers(void)
 
 	for (ro = 0; ro < 10; ro++)
 	{
-		
 		for (i = 0; i <= 14; i++)
 		{
-		
 			if (i >= 10)
 			{
 				_putchar((i / 10) + '0');
 			}
-
 			_putchar((i % 10) + '0');
 
 		}
