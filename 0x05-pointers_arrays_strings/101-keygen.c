b0cc32f8ab8a635c5a0816a389635ac0c4f0bc26
Tope Jimoh <jimohtope2012@gmail.com>
2022-12-19 13:54:57 -0800

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *
+ * checksum - executes checksum
+ * @s: input char
+ * Return: checksum
+ */
+
+unsigned long checksum(char *s)
+{
+
+	unsigned long sum = 0;
+
+	while (*s != 0)
+	{
+	sum += *s;
+	s++;
+	}
+	return (sum);
+}
+/**
+ *
+ * main - prints password for crakme
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQSTUVWXYZ";
+	char s[33];
+	unsigned long sum;
+	int t, flag = 0;
+
+	srand(time(NULL));
+	while (flag == 0)
+	{
+		for (t = 0; i < 33; i++)
+		{
+			s[i] = alpha[rand() % (sizeof(alpha) - 1)];
+		}
+		s[i] = '\0';
+		sum = checksum(s);
+		if (sum == 2772)
+		{
+			flag = 1;
+			printf("%s", s);
+		}
+	}
+	return (0);
+}
