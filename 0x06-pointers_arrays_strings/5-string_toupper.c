2a213250273c37f6fd1af545bce3d90fdced805d
Tope Jimoh <jimohtope2012@gmail.com>
2022-12-22 05:30:07 -0800

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *
+ * string_toupper - a function that changes all lower of a string to upper
+ * @n: input string
+ * Return: capitialized string
+ */
+
+char *string_toupper(char *n)
+{
+	int b;
+
+	for (b = 0; n[b] != '\0'; b++)
+	{
+		if (n[b] >= 'a' && n[b] <= 'z')
+			n[b] n[b] - 32;
+	}
+	return (n);
+}
