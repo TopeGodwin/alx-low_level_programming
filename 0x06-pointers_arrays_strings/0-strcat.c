1deb53dc983e418095bd0a9618675bf42f484da0
Tope Jimoh <jimohtope2012@gmail.com>
2022-12-22 13:49:16 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,16 +9,25 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int n, t = 0;
+	int n, m, t;
 
-	while (dest[t] != '\0' )
+	n = m = 0;
+	while (dest[n] != '\0' )
 	{
-		t++;
+		n++;
 	}
 
-	for (n = 0; (dest[t + n] = *src++) != '\0'; n++)
+	while (src[m] != '\0')
 	{
+		m++;
 	}
-
+	t = m;
+	m = 0;
+	while (m < t)
+	{
+		dest[n] = src[m];
+		n++;
+		m++;
+	}
 	return (dest);
 }
