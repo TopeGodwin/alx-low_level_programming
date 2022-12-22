a59d7ff0df03e0b2ea5fb884b8487d50c9a7beba
Tope Jimoh <jimohtope2012@gmail.com>
2022-12-22 13:36:39 -0800

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,11 +15,10 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
 
-	for (j = 0; src[j] != '\0'; j++)
-		if (i < n)
-			dest[j] = ssrc[j];
-	while (i < n)
-		dest[i++] = '\0';
+	for (j = 0; j < n && src[j] != '\0'; j++)
+			dest[j] = src[j];
+	for (; j < n; j++)
+		dest[j] = '\0';
 
 	return (dest);
 }
