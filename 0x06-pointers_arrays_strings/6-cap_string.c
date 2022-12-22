c0fb195b76f74e508c7ef820c5bb5d386a37f4df
Tope Jimoh <jimohtope2012@gmail.com>
2022-12-22 14:30:46 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,26 +7,35 @@
  */
 char *cap_string(char *n)
 {
-	int a, b, cap = 32;
-	int separations[] = {',', ';', '.', '?', '"', '(', ')', '{', ' ', '\n', '\t'};
+	int i;
 
-	for (a = 0; n[a] != '\0'; a++)
+	i = 0;
+	while (n[i] != '\0')
 	{
-		if (n[a] >= 'a' && n[a] <= 'z')
+		if (str[0] >= 'a' && str[0] <= 'z')
+			str[i] = str[i] - 32;
+		switch (str[i])
 		{
-			n[a] = n[a] - cap;
+			case ' ':
+			case '\t':
+			case '\n':
+			case ',':
+			case ';':
+			case '.':
+			case '!':
+			case '?':
+			case '"':
+			case '(':
+			case ')':
+			case '{':
+			case '}':
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				{
+					str[i + 1] = str[i + 1] - 32;
+				}
 		}
-
-		cap = 0;
-
-		for (b = 0; b <= 12; b++)
-		{
-			if (n[a] == separators[b])
-			{
-				b = 12;
-				cap = 32;
-			}
-		}
+		i++;
 	}
 	return (n);
+}
 }
