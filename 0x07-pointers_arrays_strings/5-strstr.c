d8403600111033191d0af0eb2a0e10dca1f3ca94
Tope Jimoh <jimohtope2012@gmail.com>
2023-01-02 13:11:22 -0800

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,7 +13,7 @@ char *_strstr(char *haystack, char *needle)
 	for (; *haystack != '\0'; haystack++)
 	{
 		char *one = haystack;
-		char two = needle;
+		char *two = needle;
 
 		while (*one == *two && *two != '\0')
 		{
