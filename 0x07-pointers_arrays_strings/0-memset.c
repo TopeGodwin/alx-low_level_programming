ea333d42338902de710aba00ca70008075f9af35
Tope Jimoh <jimohtope2012@gmail.com>
2023-01-02 02:49:10 -0800

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memsset - Entry point
+ * @s: will Pointed destination
+ * @b: will be constant byte
+ * @n: bytes
+ * 
+ * Return: 0 (pointer to the memory area s)
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
