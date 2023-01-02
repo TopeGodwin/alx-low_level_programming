9ba7133d0def61301e15f651821a5a301b553512
Tope Jimoh <jimohtope2012@gmail.com>
2023-01-02 03:00:30 -0800

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - Entry point 
+ * @n: input number
+ * @src: memory from input area
+ * @dest: memory to input area
+ *
+ * Return: 0 (dest)
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+	unsigned int t; 
+
+	for (t = 0; t < n; t++)
+		dest[t] = src[t];
+
+	return (dest);
+}
