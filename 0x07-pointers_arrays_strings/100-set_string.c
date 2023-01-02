3712bd6b58ef635b6ee2b980d9a5b5a4aeb7a3fb
Tope Jimoh <jimohtope2012@gmail.com>
2023-01-02 14:21:03 -0800

    100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -2,11 +2,10 @@
 
 /**
  * set_string - Write funct that set the value of pointer to a char
- * @s: input 
- * @to: input 
+ * @s: input
+ * @to: input
  * Return: Alway 0 (Success)
  */
-
 void set_string(char **s, char *to)
 {
 	*s = to;
