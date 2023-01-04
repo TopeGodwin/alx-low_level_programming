d455abf2c59d9ca2a4645b901730ecebdac21808
Tope Jimoh <jimohtope2012@gmail.com>
2023-01-04 06:25:45 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -16,8 +16,8 @@ int sqrt2(int a, int b)
 	if (b * b == a)
 		return (b);
 	else if (b * b > a)
-		return (-1);
-	return (sqrt2(a, b + 1));
+		return (- 1);
+	return (sqrt2 (a, b + 1));
 }
 
 int _sqrt_recursion(int n)
