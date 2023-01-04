3d4c6130fb7d7f398cd50489cf67161c0d088577
Tope Jimoh <jimohtope2012@gmail.com>
2023-01-04 06:35:16 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,7 +2,7 @@
 
 /**
  * _sqrt_recursion - returns the natural square root of n
- * sqrt2 - Makes possible to evaluate from 1 to n
+ * @sqrt2: Makes possible to evaluate from 1 to n
  * @n: Number Integer
  * @a: same number as n
  * @b: number that iterates from 1 to n
@@ -16,8 +16,8 @@ int sqrt2(int a, int b)
 	if (b * b == a)
 		return (b);
 	else if (b * b > a)
-		return (- 1);
-	return (sqrt2 (a, b + 1));
+		return (-1);
+	return (sqrt2(a, b + 1));
 }
 
 int _sqrt_recursion(int n)
