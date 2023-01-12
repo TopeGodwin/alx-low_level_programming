eb0268ce6e538a5a0cae0489e01a9a596262c973
Tope Jimoh <jimohtope2012@gmail.com>
2023-01-12 14:13:47 -0800

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -20,22 +20,24 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (old_size == new_size)
 		return (ptr);
 	if (ptr == 0)
+	{
 		new = malloc(new_size * sizeof(char));
-	return (new);
-
+		return (new);
+	}
 	if (new_size == 0 && ptr != 0)
+	{
 		free(ptr);
-	return (NULL);
-
+		return (NULL);
+	}
 	new = malloc(new_size * sizeof(char));
 	if (new == 0)
 		return (NULL);
 	if (new_size < old_size)
+	{
 		for (a = 0; a < new_size; a++)
 			new[a] = old[a];
-	return (new);
-
-
+		return (new);
+	}
 	free(ptr);
 	return (new);
 }
