08346e8df674757295d3ac18c87c27e04692ff54
Tope Jimoh <jimohtope2012@gmail.com>
2023-01-16 13:23:27 -0800

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees memory from allocated struct
+ * @d: input struct type
+ * Return: void function to free memory
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
