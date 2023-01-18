b9d0c42800d1210001ce25ad1f7d2d412f86be22
Tope Jimoh <jimohtope2012@gmail.com>
2023-01-18 13:24:43 -0800

    A goal is not always meant to be reached

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * get_op_func - a pointer to the function that corresponds to the op as a parm
+ *  @s: operator passed as argument
+ *  Return: Always 0 for sucess
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+	{ "+", op_add },
+	{ "-", op_sub },
+	{ "*", op_mul },
+	{ "/", op_div },
+	{ "%", op_mod },
+	{ NULL, NULL }
+	};
+	int m = 0;
+
+	while (i < 5)
+	{
+		if (strcmp(s, ops[m].op) == 0)
+			return (ops[m].f);
+
+		m++;
+	}
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - function
+ * @argc: number of arguments
+ * @argv: argument variables passed
+ * Return: Allways 0.
+ */
+
+int main(int argc, char **argv)
+{
+	int num1, num2, result;
+	int (*res)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	res = get_op_func(argv[2]);
+	if (res == 0)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	num1 == atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	result = res(num1, num2);
+	printf("%d\n", result);
+	return (0);
+}
