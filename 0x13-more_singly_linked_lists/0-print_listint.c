985c4e342f3e15277b07dbcf1bdf56b89a9c1662
TopeGodwin <jimohtope2012@gmail.com>
2023-01-31 15:07:12 -0800

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,16 @@
  */
 size_t print_listint(const listint_t *h)
 {
- size_t num = 0;
-
- while (h)
- {
- printf("%d\n", h->n);
- num++;
- h = h->next;
- }
-
- return (num);
+	size_t num = 0;
+	
+	if (!h)
+		return (0);
+	
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		num++;
+	}
+	return (num);
 }
