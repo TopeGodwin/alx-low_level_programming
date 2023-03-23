f2944c41275898e146103091b4c69170ba2a29c4
TopeGodwin <jimohtope2012@gmail.com>
2023-03-23 15:48:40 -0700

    update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -p /tmp https://github.com/TopeGodwin/alx-low_level_programming/raw/master/0x18-dynamic_libraries/gm.so
-export LD_PRELOAD=/tmp/gm.so
+wget -p .. https://raw.github.com/TopeGodwin/alx-low_level_programming/main/0x18-dynamic_libraries/gm.so
+export LD_PRELOAD="$PWD/../gm.so"
