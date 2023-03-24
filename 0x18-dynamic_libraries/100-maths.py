b1a4c9d3ec5422148c918b7dbbc83d7e268932f9
TopeGodwin <jimohtope2012@gmail.com>
2023-03-24 16:42:48 -0700

    update

--- 0x18-dynamic_libraries/100-maths.py
+++ 0x18-dynamic_libraries/100-maths.py
@@ -1,15 +1,15 @@
 from ctypes import *
-libCalc = CDLL("./libcalci.so")
+math = CDLL("./100-operations.so")
  
-#call C function to check connection
-libCalc.connect() 
+#call C function to check connection 
+math.connect() 
  
 #calling randNum() C function
 #it returns random number
-varRand = libCalc.randNum()
+varRand = math.randNum()
 print "Random Number:", varRand, type(varRand)
  
 #calling addNum() C function
 #it returns addition of two numbers
-varAdd = libCalc.addNum(20,30)
+varAdd = math.addNum(20,30)
 print "Addition : ", varAdd
